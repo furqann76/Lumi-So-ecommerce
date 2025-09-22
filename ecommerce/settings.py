e877bf67eb06dfe88f40fb1e25ee568c7e7b68f9
furqann76 <furqanbhai23456@gmail.com>
2025-09-22 12:26:57 +0500

    added jazmin in django admin

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "admin_interface",
     "colorfield",
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
